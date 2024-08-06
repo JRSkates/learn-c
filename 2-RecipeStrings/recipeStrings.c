cbb61e98facbda1412f601dd626d106605bf7829
JRSkates <skatesproduction@gmail.com>
2024-08-06 12:36:26 +0100

    syntax fixing

--- 2-RecipeStrings/recipeStrings.c
+++ 2-RecipeStrings/recipeStrings.c
@@ -3,6 +3,6 @@
 int main() {
   // Simple Recipe
   printf("2 Cups: All Purpose Flour\n");
-  printf("1 Cups: Unsalted Butter\t(Room Temperature)");
-  printf("\n2/3 Cups: Granulated Sugar");
+  printf("1 Cups: Unsalted Butter\t(Room Temperature)\n");
+  printf("2/3 Cups: Granulated Sugar\n");
 }
