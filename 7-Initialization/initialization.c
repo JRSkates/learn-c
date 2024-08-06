ab7fd0b17c9c216fc4b2c3b219f6c5515c98b471
JRSkates <skatesproduction@gmail.com>
2024-08-06 13:40:40 +0100

    initialised variables of different data types and print

---
+++ 7-Initialization/initialization.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  int numOfBooks = 12;
+  char favLetter = 'J';
+  char favDigit = '7';
+  double costOfCandyBar = 1.99;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your favourite letter is: %c\n", favLetter);
+  printf("Your favourite digit is: %c\n", favDigit);
+  printf("You expect to pay £%.2f for a candy\n", costOfCandyBar);
+
+}
