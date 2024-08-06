373bce85a641b369677cd4cf82376d3bef08d1f0
JRSkates <skatesproduction@gmail.com>
2024-08-06 12:29:29 +0100

    comments and block comments

---
+++ 3-Comments/comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // My opinion on cookies
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /* I'm learning C for my job at Sky
+  It's been great so far, hopefully I can
+  become a C expert
+  */
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
