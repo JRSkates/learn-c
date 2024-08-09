2a74babf6ee812c8444241423c7b1f07145d88f2
JRSkates <skatesproduction@gmail.com>
2024-08-09 14:45:21 +0100

    updating variable values in the control flow

---
+++ 9-UpdatingValues/updatingValues.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  char bookVersionReview = 'A';
+  char movieVersionReview = 'B';
+  double ticketPrice = 10.25;
+  double bookPrice = 19.99;
+
+  movieVersionReview = 'C';
+
+  ticketPrice = bookPrice;
+
+  printf("The book version has a review score of %c and costs £%.2f\n", bookVersionReview, bookPrice);
+  printf("The movie version has a review score of %c and costs £%.2f\n", movieVersionReview, ticketPrice);
+}
